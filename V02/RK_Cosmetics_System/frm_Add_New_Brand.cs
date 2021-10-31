672217973d7b8a3176a12a9ab27676296d580f9a
snehal renushe <snehalrenushe15@gmail.com>
2021-10-31 21:34:45 +0530

    Tables Created

---
+++ V02/RK_Cosmetics_System/frm_Add_New_Brand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RK_Cosmetics_System
+{
+    public partial class frm_Add_New_Brand : Form
+    {
+        public frm_Add_New_Brand()
+        {
+            InitializeComponent();
+        }
+
+        private void gb_Brand_Details_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Save_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ V02/RK_Cosmetics_System/frm_View_Customer_Details.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace RK_Cosmetics_System
+{
+    partial class frm_View_Customer_Details
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gb_Customer_Details = new System.Windows.Forms.GroupBox();
+            this.btn_Refresh = new System.Windows.Forms.Button();
+            this.dgv_View_Customer_Details = new System.Windows.Forms.DataGridView();
+            this.pnl_Customer_Details = new System.Windows.Forms.Panel();
+            this.btn_Search = new System.Windows.Forms.Button();
+            this.lbl_From_Date = new System.Windows.Forms.Label();
+            this.dtp_From_Date = new System.Windows.Forms.DateTimePicker();
+            this.dtp_To_Date = new System.Windows.Forms.DateTimePicker();
+            this.lbl_To_Date = new System.Windows.Forms.Label();
+            this.gb_Customer_Details.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_View_Customer_Details)).BeginInit();
+            this.pnl_Customer_Details.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gb_Customer_Details
+            // 
+            this.gb_Customer_Details.Controls.Add(this.btn_Refresh);
+            this.gb_Customer_Details.Controls.Add(this.dgv_View_Customer_Details);
+            this.gb_Customer_Details.Controls.Add(this.pnl_Customer_Details);
+            this.gb_Customer_Details.Font = new System.Drawing.Font("Modern No. 20", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gb_Customer_Details.Location = new System.Drawing.Point(22, 72);
+            this.gb_Customer_Details.Name = "gb_Customer_Details";
+            this.gb_Customer_Details.Size = new System.Drawing.Size(1862, 794);
+            this.gb_Customer_Details.TabIndex = 1;
+            this.gb_Customer_Details.TabStop = false;
+            this.gb_Customer_Details.Text = "Customer Details";
+            // 
+            // btn_Refresh
+            // 
+            this.btn_Refresh.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Refresh.BackColor = System.Drawing.Color.Yellow;
+            this.btn_Refresh.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Refresh.ForeColor = System.Drawing.Color.Black;
+            this.btn_Refresh.Location = new System.Drawing.Point(859, 695);
+            this.btn_Refresh.Name = "btn_Refresh";
+            this.btn_Refresh.Size = new System.Drawing.Size(138, 56);
+            this.btn_Refresh.TabIndex = 30;
+            this.btn_Refresh.Text = "Refresh";
+            this.btn_Refresh.UseVisualStyleBackColor = false;
+            // 
+            // dgv_View_Customer_Details
+            // 
+            this.dgv_View_Customer_Details.BackgroundColor = System.Drawing.Color.PeachPuff;
+            this.dgv_View_Customer_Details.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_View_Customer_Details.Location = new System.Drawing.Point(46, 206);
+            this.dgv_View_Customer_Details.Name = "dgv_View_Customer_Details";
+            this.dgv_View_Customer_Details.RowTemplate.Height = 28;
+            this.dgv_View_Customer_Details.Size = new System.Drawing.Size(1775, 457);
+            this.dgv_View_Customer_Details.TabIndex = 29;
+            // 
+            // pnl_Customer_Details
+            // 
+            this.pnl_Customer_Details.BackColor = System.Drawing.Color.MistyRose;
+            this.pnl_Customer_Details.Controls.Add(this.btn_Search);
+            this.pnl_Customer_Details.Controls.Add(this.lbl_From_Date);
+            this.pnl_Customer_Details.Controls.Add(this.dtp_From_Date);
+            this.pnl_Customer_Details.Controls.Add(this.dtp_To_Date);
+            this.pnl_Customer_Details.Controls.Add(this.lbl_To_Date);
+            this.pnl_Customer_Details.Location = new System.Drawing.Point(138, 44);
+            this.pnl_Customer_Details.Name = "pnl_Customer_Details";
+            this.pnl_Customer_Details.Size = new System.Drawing.Size(1590, 100);
+            this.pnl_Customer_Details.TabIndex = 0;
+            // 
+            // btn_Search
+            // 
+            this.btn_Search.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Search.BackColor = System.Drawing.Color.Yellow;
+            this.btn_Search.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Search.ForeColor = System.Drawing.Color.Black;
+            this.btn_Search.Location = new System.Drawing.Point(1333, 23);
+            this.btn_Search.Name = "btn_Search";
+            this.btn_Search.Size = new System.Drawing.Size(138, 56);
+            this.btn_Search.TabIndex = 28;
+            this.btn_Search.Text = "Search";
+            this.btn_Search.UseVisualStyleBackColor = false;
+            
+            // 
+            // lbl_From_Date
+            // 
+            this.lbl_From_Date.AutoSize = true;
+            this.lbl_From_Date.BackColor = System.Drawing.Color.Pink;
+            this.lbl_From_Date.Font = new System.Drawing.Font("Microsoft YaHei", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_From_Date.ForeColor = System.Drawing.Color.Maroon;
+            this.lbl_From_Date.Location = new System.Drawing.Point(73, 32);
+            this.lbl_From_Date.Name = "lbl_From_Date";
+            this.lbl_From_Date.Size = new System.Drawing.Size(155, 36);
+            this.lbl_From_Date.TabIndex = 15;
+            this.lbl_From_Date.Text = "From Date";
+            // 
+            // dtp_From_Date
+            // 
+            this.dtp_From_Date.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtp_From_Date.Location = new System.Drawing.Point(257, 32);
+            this.dtp_From_Date.Name = "dtp_From_Date";
+            this.dtp_From_Date.Size = new System.Drawing.Size(301, 39);
+            this.dtp_From_Date.TabIndex = 21;
+            // 
+            // dtp_To_Date
+            // 
+            this.dtp_To_Date.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtp_To_Date.Location = new System.Drawing.Point(870, 32);
+            this.dtp_To_Date.Name = "dtp_To_Date";
+            this.dtp_To_Date.Size = new System.Drawing.Size(301, 39);
+            this.dtp_To_Date.TabIndex = 23;
+            // 
+            // lbl_To_Date
+            // 
+            this.lbl_To_Date.AutoSize = true;
+            this.lbl_To_Date.BackColor = System.Drawing.Color.Pink;
+            this.lbl_To_Date.Font = new System.Drawing.Font("Microsoft YaHei", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_To_Date.ForeColor = System.Drawing.Color.Maroon;
+            this.lbl_To_Date.Location = new System.Drawing.Point(686, 32);
+            this.lbl_To_Date.Name = "lbl_To_Date";
+            this.lbl_To_Date.Size = new System.Drawing.Size(119, 36);
+            this.lbl_To_Date.TabIndex = 22;
+            this.lbl_To_Date.Text = "To Date";
+            // 
+            // frm_View_Customer_Details
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(1920, 918);
+            this.ControlBox = false;
+            this.Controls.Add(this.gb_Customer_Details);
+            this.Name = "frm_View_Customer_Details";
+            this.Text = "View Customer Details";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.gb_Customer_Details.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_View_Customer_Details)).EndInit();
+            this.pnl_Customer_Details.ResumeLayout(false);
+            this.pnl_Customer_Details.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gb_Customer_Details;
+        private System.Windows.Forms.Label lbl_From_Date;
+        private System.Windows.Forms.DateTimePicker dtp_To_Date;
+        private System.Windows.Forms.Label lbl_To_Date;
+        private System.Windows.Forms.DateTimePicker dtp_From_Date;
+        private System.Windows.Forms.Button btn_Search;
+        private System.Windows.Forms.Panel pnl_Customer_Details;
+        private System.Windows.Forms.Button btn_Refresh;
+        private System.Windows.Forms.DataGridView dgv_View_Customer_Details;
+    }
+}
